2204165c7ac958cafbc9ff2d1a8f88930d32fb21
VStorm <tranvubao2004@gmail.com>
2025-08-18 19:47:54 +0700

    update to mvc

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST_NAME || 'localhost';
+
+// config template engine (EJS)
+// app.set('views', './src/views');
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+// config static files
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get("/", (req, res) => {
+    res.render('sample.ejs')
+});
+
+app.listen(port, hostname, () => {
+    console.log(`example app listening on port ${port}`)
+});
