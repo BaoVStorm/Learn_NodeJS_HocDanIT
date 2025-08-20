769ea3c08f39ff492b793b297abdd1c35838a2bb
VStorm <tranvubao2004@gmail.com>
2025-08-20 12:07:32 +0700

    change connect database to pooling

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 // create connection to database (mysql)
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT, // default: 3306
+// });
+
+// create connection to database (mysql)
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT, // default: 3306
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection;
